ba567a522d2d42064b7c470f942aa97336318d62
Gerardo G G <areg_4@hotmail.com>
2016-11-16 13:00:25 -0600

    commit 01

---
+++ src/com/everis/TallerJava/TiposDatos/TiposDeDatos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.everis.TallerJava.TiposDatos;
+
+/**
+ *
+ * @author Berserker
+ */
+public class TiposDeDatos {
+    public static void main(String[] args) {
+        char a = 'h';
+        String nombre = "Nombre";
+        byte miByte = 127;
+        short entero2 = 23;
+        int entero = 1111111;
+        long entero4 = 121;
+        
+        float tipoFlotante = 11f;
+        double tipoFlotante2 = 11;
+        
+        boolean booleano = true;
+        boolean booleano2 = false;
+    }
+}
